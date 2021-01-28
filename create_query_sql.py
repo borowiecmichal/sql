888748fe20142afad2a0ad6f8ad418b515170476
Michal Borowiec <michalborowiec@op.pl>
2021-01-28 11:36:49 +0100

    created flask site

--- create_query_sql.py
+++ create_query_sql.py
@@ -3,7 +3,7 @@ CREATE TABLE author(
 id serial,
 first_name varchar(20),
 last_name varchar (20),
-PRIMARY KEY (id))
+PRIMARY KEY (id));
 """
 
 book_query = """
@@ -12,7 +12,7 @@ id serial,
 title varchar(30),
 id_author integer,
 PRIMARY KEY (id),
-FOREIGN KEY (id_author) REFERENCES author(id))
+FOREIGN KEY (id_author) REFERENCES author(id));
 """
 
 creation_query_list = [author_query, book_query]
---
+++ start.py
@@ -0,0 +1,11 @@
+from connection import connect
+from flask import Flask, request,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template("base.html")
+
+if __name__ == '__main__':
+    app.run()
